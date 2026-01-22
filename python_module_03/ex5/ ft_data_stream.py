8994fc3d81fa241bcebe906d56a2997e524e355f
nicowers <118110800+nicowers@users.noreply.github.com>
2026-01-22 16:57:24 +0100

    Add files via upload

--- python_module_03/ex5/ ft_data_stream.py
+++ python_module_03/ex5/ ft_data_stream.py
@@ -1,8 +1,56 @@
 #!/usr/bin/env python3
+import time
 
-def event():
-    players = ["alice", "bob", "charlie", "david", "ethann", "faustin"]
-    events = {"killed monster", "found treasure", "leveled up"}
+def event(nbr_event):
+    players = ("alice", "bob", "charlie", "david", "ethann", "faustin")
+    events = ("killed monster",
+            "found treasure",
+            "leveled up",
+            "killed monster",
+            "killed monster",
+            "killed monster",
+            "killed monster",
+            "killed monster",
+            "leveled up",
+            "killed monster",
+            "killed monster",
+            "killed monster",
+            "killed monster"
+            )
+    player_lvl = [5, 12, 8, 7, 3, 1]
+    for i in range(nbr_event):
+        index = (i % len(players))
+        event_index = (i % len(events))
+        if events[event_index]== "leveled up":
+            player_lvl[index] += 1
+        yield {
+            "id" : i + 1,
+            "player_level" : player_lvl[index],
+            "player" : players[index],
+            "event" : events[event_index]
+            }
+
+def stream_analytics(nbr_event, display=3):
+    high_level_players = treasure_events = level_up_events = 0
+    print(f"\nProcessing {nbr_event} game events...\n")
+    for i, events in enumerate(event(nbr_event)):
+        if events["event"] == "found treasure":
+            treasure_events += 1
+        if events["player_level"] >= 10:
+            high_level_players += 1
+        if events["event"] == "leveled up":
+            level_up_events += 1
+        if i < display :
+            print(f"Event {events['id']}: Player {events['player']} (level {events['player_level']}) killed monster")
+    else:
+        print("...")
+    print("\n=== Stream Analytics ===")
+    print(f"Total events processed: {nbr_event}")
+    print(f"High-level players (10+): {high_level_players}")
+    print(f"Treasure events: {treasure_events}")
+    print(f"Level-up events: {level_up_events}")
+    print("\nMemory usage: Constant (streaming)")
+
 
 def fibonacci():
     a, b = 0, 1
@@ -24,13 +72,14 @@ def prime(n):
             count += 1
         number += 1
 
-
 if __name__ == "__main__":
     n = 1000
     print("=== Game Data Stream Processor ===")
+    start = time.time()
+    stream_analytics(n)
+    end = time.time()
+    print(f"Processing time: {end - start:.6f} seconds")
     print("\n=== Generator Demonstration ===")
-    print(f"\nProcessing {n} game events...\n")
-    print("\n=== Stream Analytics ===")
     prime_len = 5
     prime_list = []
     prime_number = prime(10)
