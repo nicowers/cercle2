f28f7bc564882b95101f2cdc40d96068ad16a740
nicowers <118110800+nicowers@users.noreply.github.com>
2026-01-23 11:28:20 +0100

    Delete python_module_00 directory

--- python_module_00/ex5/ft_count_harvest_iterative.py
+++
@@ -1,6 +0,0 @@
-def ft_count_harvest_iterative():
-    remaining_days = int(input("Days until harvest: "))
-    for i in range(remaining_days):
-        i += 1
-        print("Day", i)
-    print("Harvest time!")
--- python_module_00/ex5/ft_count_harvest_recursive.py
+++
@@ -1,8 +0,0 @@
-def ft_count_harvest_recursive(days=None, count=1):
-    if days is None:
-        days = int(input("Days until harvest: "))
-    if days == 0:
-        print("Harvest Time!")
-    else:
-        print(f"Day {count}")
-        return ft_count_harvest_recursive(days - 1, count + 1)
