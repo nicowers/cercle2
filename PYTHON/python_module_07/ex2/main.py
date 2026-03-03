20510cafec9cbf28b04491e01edeb67c1cd6c232
nicola <nicola.debavelaere2005@gmail.com>
2026-03-03 09:42:22 +0100

    test

---
+++ PYTHON/python_module_07/ex2/main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("\n=== DataDeck Ability System ===\n")
+    print("EliteCard capabilities:")
+    print("- Card: ['play', 'get_card_info', 'is_playable']")
+    print("- Combatable: ['attack', 'defend', 'get_combat_stats']")
+    print("- Magical: ['cast_spell', 'channel_mana', 'get_magic_stats']")
